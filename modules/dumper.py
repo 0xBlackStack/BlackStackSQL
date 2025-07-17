46cd821b953508292a1b974224c0d15ed42f1b8c
Reflex <sparsh5.5chaudhary@gmail.com>
2025-07-17 20:45:45 +0530

    Update README.md

--- modules/dumper.py
+++ modules/dumper.py
@@ -9,10 +9,11 @@ class DatabaseDumper:
         self.headers = headers or {}
         self.data = data
 
-    def dump_database(self, columns=None):
+    def dump_database(self, columns=None, proxies=None):
         # Implement database dumping logic
+        # Use proxies if provided
         pass
 
-def dump_database(url, headers=None, data=None, columns=None):
+def dump_database(url, headers=None, data=None, columns=None, proxies=None):
     dumper = DatabaseDumper(url, headers, data)
-    dumper.dump_database(columns)
+    dumper.dump_database(columns, proxies)
